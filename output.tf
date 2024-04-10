ad82a72cde8eed3da41e3efc7177fed78b8b2ad9
naresh <Naresh@gmail.com>
2024-04-10 10:41:21 +0530

    aws vpc module devolped

--- output.tf
+++ output.tf
@@ -1,6 +1,15 @@
-# output "output_cidrblock" {
-#     value = aws_vpc.myvpc.cidr_block
-#   }
-# output "output_EIP" {
-#   value = aws_eip.eip
-# }
+output "EIP_ip" {
+  value = aws_eip.eip.address
+}
+output "public_ip" {
+    value = aws_subnet.publicsubnet.cidr_block
+}
+output "private_ip" {
+    value = aws_subnet.privatesubnet.cidr_block
+}
+output "db_ip" {
+    value = aws_subnet.dbsubnet.cidr_block
+}
+output "vpc_id" {
+    value = aws_vpc.myvpc.id
+}
