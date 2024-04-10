8e29346dfa02cf44f817677623ed21f260f89c0f
naresh <Naresh@gmail.com>
2024-04-10 20:54:30 +0530

    added

--- output.tf
+++ output.tf
@@ -6,11 +6,14 @@
 #     value = aws_subnet.publicsubnet[count.index].cidr_block
 # }
 # output "private_ip" {
-#     value = aws_subnet.privatesubnet[count.index].cidr_block
+# value = aws_subnet.privatesubnet[count.index].cidr_block
 # }
 # output "db_ip" {
-#     value = aws_subnet.dbsubnet[count.index].cidr_block
+# value = aws_subnet.dbsubnet[count.index].cidr_block
 # }
 output "vpc_id" {
     value = aws_vpc.myvpc.id
+}
+output "public_subnet_ids" {
+    value = aws_subnet.publicsubnet[*].id
 }
